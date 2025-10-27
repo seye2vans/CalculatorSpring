3b47e7c8d843f685e3dda3878e4648972b783682
Victor Olaleye <vansvictor79@gmail.com>
2025-10-27 17:17:00 +0100

    Added 2 files

--- calculator/src/test/java/com/example/calculator/CalculatorApplicationTests.java
+++ calculator/src/test/java/com/example/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.calculator;
 
+import com.example.calculator.Service.CalculatorService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class CalculatorApplicationTests {
+public class CalculatorApplicationTests {
+
+	@Autowired
+	private CalculatorService calculatorService;
 
 	@Test
-	void contextLoads() {
+	public void testSum() {
+		int result = calculatorService.sum(2, 3);
+		assertEquals(5, result);
 	}
-
 }
